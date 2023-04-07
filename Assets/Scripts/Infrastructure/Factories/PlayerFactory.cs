05bf25f69da68b1054a0c035f3a3e6e92afce358
tetanurae <ivan.kozlov.dev@gmail.com>
2023-04-07 18:29:55 +0300

    Clean unused classes and code

--- Assets/Scripts/Infrastructure/Factories/PlayerFactory.cs
+++ Assets/Scripts/Infrastructure/Factories/PlayerFactory.cs
@@ -18,19 +18,7 @@ namespace ShapesGame.Infrastructure.Factories
         public GameObject Create(Vector3 position)
         {
             var prefab = _assets.Get(AssetPath.Player);
-            var player = _diContainer.InstantiatePrefab(prefab, position, Quaternion.identity, null);
-
-            // var playerMover = player.GetComponent<PlayerMover>();
-            // playerMover.Init(_inputService, _staticDataService.PlayerData, _pauseService);
-            // playerMover.SetCamera(_camera);
-            //
-            // var targetTrigger = player.GetComponentInChildren<TargetTrigger>();
-            // targetTrigger.Init(_popupService, _staticDataService.GameData, _inputService, _quizGameLauncher, _pauseService);
-            //
-            // var collector = player.GetComponent<ShapesCollector>();
-            // collector.Init(_staticDataService);
-            
-            return player;
+            return _diContainer.InstantiatePrefab(prefab, position, Quaternion.identity, null);
         }
     }
 }
